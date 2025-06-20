c00e5400ecb86a861cd8b1380abaf56c4aa8f7b1
Balanyuk Nikita <NekitGraviton2002@gmail.com>
2025-06-20 22:11:34 +0300

    init bot

---
+++ index.js
@@ -0,0 +1,12 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.text.toLowerCase() === 'привет') {
+    bot.sendMessage(chatId, 'Привет, я живой!');
+  }
+});
